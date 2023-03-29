eabf70869a6b5320369f14e1ad4bafb9b8ef9be9
Isaaclkm <isaacfelixmatos@gmail.com>
2023-03-29 07:55:09 -0400

    done

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Components/Sidebar/Sidebar.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar/>
       <h1 className="text-3xl font-bold underline">
       Hello world!
       </h1>
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['plus-jakarta-sans', 'system-ui'],
+        'serif': ['ui-serif', 'Georgia'],
+        'mono': ['ui-monospace', 'SFMono-Regular'],
+        'display': ['Oswald'],
+        'body': ['"Open Sans"'],
+      },
+    },
+    colors: {
+      primary: ['#3E3F4E'],
+    }
   },
   plugins: [],
 }
