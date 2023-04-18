5c0b7fe243825e91552afea8cf736ea10e364af7
Franklin Isaac Felix Matos <isaacfelixmatos@gmail.com>
2023-04-17 22:07:27 -0400

    trying to finish this

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import { ApolloServer } from 'apollo-server';
+
+
 const app = express();
 import { graphqlHTTP } from 'express-graphql';
 import { schema } from './Schema/schema.js';
 
 import { connect } from './db.js';
 
+const server = new ApolloServer({ 
+  schema
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
 
 const PORT = process.env.PORT || 5000; // Set the port to listen on
 
