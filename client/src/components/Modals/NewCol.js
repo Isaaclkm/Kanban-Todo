db2036376ab079935524907c849d85b5a6167be6
Franklin Isaac Felix Matos <isaacfelixmatos@gmail.com>
2023-05-04 22:13:33 -0400

    done it

--- client/src/components/Modals/NewCol.js
+++ client/src/components/Modals/NewCol.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react'
 import './Modal.css'
 import { useMutation, gql } from '@apollo/client'
 
+
+
+const NewCol = ({onClose, projectId}) => {
+const [title, setTitle] = useState('');
+
 const CREATE_COLUMN_MUTATION = gql`
   mutation($title: String!, $projectId: ID!){
   createColumn(title: $title, projectId: $projectId) {
@@ -9,9 +14,6 @@ const CREATE_COLUMN_MUTATION = gql`
     title
   }
 }`
-
-const NewCol = ({onClose, projectId}) => {
-const [title, setTitle] = useState('');
 
 const [createColumn, { data, loading, error }] = useMutation(CREATE_COLUMN_MUTATION);
   if (loading) return 'Submitting...';
@@ -40,10 +42,10 @@ const [createColumn, { data, loading, error }] = useMutation(CREATE_COLUMN_MUTAT
       <form onSubmit={handleSubmit}>
         <div class="flex flex-wrap -mx-3 mb-6 text-slate-100 ">
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="name">
-                Title
-            </label>
-            <input class="appearance-none block w-96 bg-primary border border-gray focus:border-morado focus:outline-none rounded py-3 px-4 mb-3 leading-tight" id="title" type="text" name="name" value={title} placeholder="" onChange={(e) => setTitle(e.target.value)}/>
+              <label class="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="name">
+                  Title
+              </label>
+              <input class="appearance-none block w-96 bg-primary border border-gray focus:border-morado focus:outline-none rounded py-3 px-4 mb-3 leading-tight" id="title" type="text" name="name" value={title} placeholder = "" onChange={(e) => setTitle(e.target.value)}/>
             </div>
         </div>
         <button className='rounded-full bg-white w-96 h-12 text-morado font-semibold' >Add new Column</button>
