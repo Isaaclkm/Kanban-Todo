9b240e5a8834f11a7038c8e3ed02cf6550a4348a
Franklin Isaac Felix Matos <isaacfelixmatos@gmail.com>
2023-05-09 22:28:38 -0400

    added the delete button

--- client/src/components/Modals/DeleteBoard.js
+++ client/src/components/Modals/DeleteBoard.js
@@ -1,7 +1,31 @@
 import React from 'react'
 import './Modal.css'
+import { useMutation, gql } from '@apollo/client';
+import { useParams } from 'react-router-dom';
 
 const DeleteBoard = ({onClose}) => {
+
+  const {id} = useParams();
+
+  const DELETE_BOARD_MUTATION = gql`
+      mutation($id: ID!){
+      deleteProject(_id: $id) {
+      _id
+    }
+  }
+  `
+  const [deleteProject, { data, loading, error }] = useMutation(DELETE_BOARD_MUTATION);
+
+  const handleSubmit = (e) => {
+
+    deleteProject({
+      variables:{
+        id
+      }
+    })
+    window.location.href = 'http://localhost:3000';
+  }
+
   return (
     <div className='Colmodal modalOverlay overflow-y-hidden overflow-x-hidden bg-primary p-6 text-slate-100 scrollbar-hide md:scrollbar-default'> 
         <div className  ="flex flex-wrap mx-3 mb-6  ">
@@ -12,7 +36,7 @@ const DeleteBoard = ({onClose}) => {
         </div>
 
        <div className='flex flex-row justify-between'> 
-            <button className='rounded-full bg-red-400 w-48 h-12 text-slate-100 font-semibold'>Delete</button>
+            <button className='rounded-full bg-red-400 w-48 h-12 text-slate-100 font-semibold ' onClick={handleSubmit}>Delete</button>
             <button className='rounded-full bg-white w-48 h-12 text-morado font-semibold' onClick={onClose}>Cancel</button>
         </div> 
     </div>
