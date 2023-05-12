5faa8b77e0d72859def8d4fbb8281e31dea7346c
Franklin Isaac Felix Matos <isaacfelixmatos@gmail.com>
2023-05-11 20:58:13 -0400

    again

--- client/src/components/Column.js
+++ client/src/components/Column.js
@@ -26,15 +26,16 @@ const  Column = ({ column }) => {
           </div>
 
          {tasks.map(task => (
-          <button>
+          
           <CardItem key={task._id} task={task} onClick={handleCardItemClick}/>
-          </button>
+    
             ))}
 
          {showTaskModal && createPortal(
              <TaskModal onClose={() => {setShowTaskModal(!showTaskModal); setShowOverlay(!showOverlay)}} />,
             document.body
          )}
+
 
 
           {/* Overlay style */}
