eb610e77e9c4fe0b22e569416f4b4a8248ab43e3
Franklin Isaac Felix Matos <isaacfelixmatos@gmail.com>
2023-05-21 14:01:30 -0400

    thanks

--- server/index.js
+++ server/index.js
@@ -2,33 +2,30 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { typeDefs, resolvers } from './Schema/schema.js';
 import cors from 'cors';
-import { connect } from './db.js';
 
 const app = express();
 
 const corsOptions = {
   origin: 'https://kanban-todos-api.onrender.com',
-  credentials: true, // if you need to include cookies or authorization headers
+  credentials: true,
 };
+
 app.use(cors(corsOptions));
 
-
-const server = new ApolloServer({ 
+const server = new ApolloServer({
   typeDefs,
   resolvers,
-  introspection: true, // Enable introspection for Apollo Server
+  introspection: true,
 });
 
-server.applyMiddleware({ app, path: '/graphql' }); // Add this line to set up the '/graphql' endpoint
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
+}
 
-const PORT = process.env.PORT || 5000; // Set the port to listen on
-
-connect();
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}graphql`);
+startServer().then(() => {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
 });
