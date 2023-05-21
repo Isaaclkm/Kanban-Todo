0875755a751ef546db737986942d7fd20f15902a
Franklin Isaac Felix Matos <isaacfelixmatos@gmail.com>
2023-05-21 16:32:20 -0400

    did it

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
+const dbPassword = process.env.DB_PASSWORD;
 
 mongoose.set('strictQuery', false);
 
 export async function connect() {
   try {
-    await mongoose.connect('mongodb+srv://Isaaclkm:KVg1HCky3dSPwZAD@cluster0.sp9rcoe.mongodb.net/<database>?retryWrites=true&w=majority', {
+    await mongoose.connect(`mongodb+srv://Isaaclkm:${dbPassword}@cluster0.sp9rcoe.mongodb.net/<database>?retryWrites=true&w=majority`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
